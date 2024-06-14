60012bda0860144ae27f3be61a8dd7b1b1e6a367
matheus <matheussancho13@gmail.com>
2024-06-13 23:52:56 -0300

    Go-Library/Add: Fix Errors and Checking

--- Section-2/3-error-checking/main.go
+++ Section-2/3-error-checking/main.go
@@ -26,18 +26,19 @@ func addValues(x, y int) int {
 	return sum
 }
 
-func Devide(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Devide Page!!")
-	f, err := devideValues(100.0, 10.2)
+func Divide(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Divide Page!!")
+	f, err := divideValues(100.0, 10.2)
 	if err != nil {
-		fmt.Fprintf(w, "cannot devide by 0")
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
 	}
-	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 10.2, f))
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.2, f))
 }
 
-func devideValues(x, y float32) (float32, error) {
+func divideValues(x, y float32) (float32, error) {
 	if y <= 0.0 {
-		err := errors.New("Cannot devide by 0")
+		err := errors.New("Cannot divide by 0")
 		return 0, err
 	}
 	result := x / y
@@ -49,10 +50,9 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/devide", Devide)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting the application on port: %s", portNumber))
 
 	_ = http.ListenAndServe(portNumber, nil)
-
 }
